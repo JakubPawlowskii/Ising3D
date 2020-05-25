81b48186ab7b36496b081b2d994def27946adf94
Jakub <kuba.pawlowski99@gmail.com>
2020-05-26 00:39:06 +0200

    3D Ising model without external field framework

---
+++ ising_3D/ising_framework.cpp
@@ -0,0 +1,99 @@
+﻿#include "ising_framework.h"
+
+using namespace std;
+
+random_device device;
+
+static inline uint64_t rotl(const uint64_t x, const int k)
+{
+	return (x << k) | (x >> (64 - k));
+}
+
+
+static uint64_t s[4] = {
+	(static_cast<uint64_t>(device()) << 32) | device(), (static_cast<uint64_t>(device()) << 32) | device(),
+	(static_cast<uint64_t>(device()) << 32) | device(), (static_cast<uint64_t>(device()) << 32) | device()
+};
+
+
+uint64_t next()
+{
+	const uint64_t result = s[0] + s[3];
+
+	const uint64_t t = s[1] << 17;
+
+	s[2] ^= s[0];
+	s[3] ^= s[1];
+	s[1] ^= s[2];
+	s[0] ^= s[3];
+
+	s[2] ^= t;
+
+	s[3] = rotl(s[3], 45);
+
+	return result;
+}
+
+static double to_double(uint64_t x)
+{
+	const union
+	{
+		uint64_t i;
+		double d;
+	} u = {UINT64_C(0x3FF) << 52 | x >> 12};
+	return u.d - 1.0;
+}
+
+
+unsigned int ising_framework::index(const unsigned int x, const unsigned int y, const unsigned int z) const
+{
+	return x + lattice_dim_ * (y + lattice_dim_ * z);
+}
+
+void ising_framework::initialize_lattice() const
+{
+	for (unsigned int i = 0; i < lattice_vol_; i++) lattice_[i] = to_double(next()) <= initial_spin_up_ ? 1 : -1;
+}
+
+void ising_framework::print_lattice_to_file(const string file_name) const
+{
+	ofstream lattice_file;
+	lattice_file.open(file_name);
+	for (unsigned i = 0; i < lattice_dim_; i++)
+		for (unsigned j = 0; j < lattice_dim_; j++)
+			for (unsigned k = 0; k < lattice_dim_; k++)
+				lattice_file << i << ";" << j << ";" << k << ";" << lattice_[index(i, j, k)] << endl;
+}
+
+void ising_framework::spin_swap_energy_difference(const unsigned int x, const unsigned int y, const unsigned int z)
+{
+	dE_ = 2 * lattice_[index(x, y, z)] * (lattice_[index(pbc_forward_[x], y, z)] + lattice_[index(
+			pbc_backward_[x], y, z)] +
+		lattice_[index(x, pbc_forward_[y], z)] + lattice_[index(x, pbc_backward_[y], z)] + lattice_[
+			index(x, y, pbc_forward_[z])] + lattice_[index(x, y, pbc_backward_[z])]);
+}
+
+double ising_framework::swap_probability() const
+{
+	return  exp(static_cast<double>(-dE_)/T_);
+}
+
+double ising_framework::magnetization() const
+{
+	auto sum = 0.0;
+	for(unsigned int i = 0; i < lattice_vol_; i++) sum+=lattice_[i];
+	return 1/static_cast<double>(lattice_vol_) *sum;
+}
+
+void ising_framework::simulate_mcs()
+{
+	for (unsigned i = 0; i < lattice_dim_; i++)
+		for (unsigned j = 0; j < lattice_dim_; j++)
+			for (unsigned k = 0; k < lattice_dim_; k++)
+			{
+				spin_swap_energy_difference(i,j,k);
+				if(dE_ <= 0 ||  to_double(next()) < swap_probability()) lattice_[index(i,j,k)] *= -1;
+			}
+}
+
+
---
+++ ising_3D/ising_framework.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include <random>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//Flat[x + WIDTH * (y + DEPTH * z)] = Original[x, y, z]
+
+class ising_framework
+{
+private:
+	unsigned int initial_spin_up_ = 0;
+	unsigned int lattice_dim_ = 0;
+	unsigned int lattice_vol_ = 0;
+	int *lattice_ = nullptr;
+	int dE_ = 0;
+	int si_ = 0, sj_ = 0;
+	double T_ = 0.0;
+	unsigned int *pbc_forward_; // periodic boundary conditions in forward direction
+	unsigned int *pbc_backward_; // periodic boundary conditions in backward direction
+public:
+	ising_framework(unsigned int n, unsigned int p, double t):
+	initial_spin_up_(p),
+	lattice_dim_(n),
+	lattice_vol_(n*n*n),
+	T_(t)
+	{
+		lattice_ = new int[lattice_vol_];
+		initialize_lattice();
+
+		pbc_forward_ = new unsigned int[lattice_dim_];
+		pbc_backward_ = new unsigned int[lattice_dim_];
+		
+		for(unsigned int i = 0; i < lattice_dim_; i++) 
+		{
+			pbc_backward_[i] = i-1;
+			pbc_forward_[i] = i+1;
+		}
+		pbc_backward_[0] = lattice_dim_-1;
+		pbc_forward_[lattice_dim_-1] = 0;
+	}
+	
+	~ising_framework()
+	{
+		delete[] lattice_;
+		delete[] pbc_backward_;
+		delete[] pbc_forward_;
+		lattice_ = nullptr;
+		pbc_backward_ = nullptr;
+		pbc_forward_ = nullptr;
+	}
+	
+	unsigned int index(unsigned int x, unsigned int y, unsigned int z) const;
+	void initialize_lattice() const;
+	void print_lattice_to_file(const string file_name) const;
+	void spin_swap_energy_difference(unsigned int x, unsigned int y, unsigned int z);
+	double swap_probability() const; //TODO:: put possible values in an array
+	double magnetization() const;
+	void simulate_mcs();
+};
